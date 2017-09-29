7e79b6be2e4b6fa65f894a6254ee1540ce5de974
Ricardo Saumeth <Ricardo.Saumeth@icsasoftware.com>
2017-09-29 13:47:20 +0100

    finished auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '682267437812-45m7qu85r40unebq36njp5r99m561bli.apps.googleusercontent.com',
+  googleClientSecret: 'PAOGDr6XJVBAASbansIop3-0',
+  mongoURL: 'mongodb://ricardo:letmein@ds151544.mlab.com:51544/emailydev',
+  cookieKey: '132asdfdgfgsdfhw13213wertrtew2Trwert3216wert23er1twer32tew'
+};
