1cc7aa63aa27d9a70045d059e094d7c6f7c36cde
Ricardo Manuel Santana Saumeth <ricardosantana46@hotmail.com>
2017-10-02 23:09:55 +0100

    Added billing and client side app!

--- index.js
+++ index.js
@@ -12,16 +12,26 @@ mongoose.connect(keys.mongoURL);
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey]
-})); // ver video 40 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+); // ver video 40
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;// using destructuring!
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
