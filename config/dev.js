694782178edad65840c29f1b3d3b9cd892f17076
Ricardo Saumeth <Ricardo.Saumeth@icsasoftware.com>
2017-10-06 17:25:07 +0100

    React Form added

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,6 @@ module.exports = {
   cookieKey: '132asdfdgfgsdfhw13213wertrtew2Trwert3216wert23er1twer32tew',
   stripePublishableKey: 'pk_test_AJ1lV09EXNhdMozL5opobE0j',
   stripeSecretKey: 'sk_test_L2r81cRT7mzXEx5GNwwyeUwk',
-  sendGridKey: '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'
+  sendGridKey: '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',
+  redirectDomain: 'http://localhost:3000'
 };
